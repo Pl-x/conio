2f624f7cf36fc1bc67d033efded9127b0760c4e0
Pl-x <148755759+Pl-x@users.noreply.github.com>
2024-09-20 18:54:09 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def install_packages(packages):
+    for package in packages:
+        try:
+            subprocess.check_call(["sudo", "apt", "install", package])
+            print(f"{package} installed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing {package}:  
+ {e}")
+
+if __name__ == "__main__":
+    packages = [
+        "net-tools",
+        "git",
+        "apache2",
+        "openssh-server",
+        "ufw",
+        "iproute2",
+        "mysql-server",
+        "tree",
+        "zip",
+        "unzip",
+        "tar",
+        "redis-server",
+        "fail2ban"
+    ]
+
+    install_packages(packages)
