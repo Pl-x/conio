f48c82f9232f941c8dcae8ea4e87b6518796303a
Pl-x <148755759+Pl-x@users.noreply.github.com>
2024-09-28 14:56:50 +0300

    fresh start

---
+++ listener.py
@@ -0,0 +1,24 @@
+import systemd
+import numpy
+import random
+def create_gate(random.rand)
+set gate = 127.0.0.1
+try
+{
+  gate.start(int random)
+     {
+       for(int w=0;w<200;w++)
+         {
+           apt install blackjack(url:https://github.com/conio/newdask.git)
+from sysmd_import crash{}
+    
+}
+
+}
+}
+
+except as reliance{
+    end.script
+        f"print("error code 1")
+}
+create_gate(random.rand)
