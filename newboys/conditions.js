5c32fcb854ba146dc08e0aed26c804a66876a352
Pl-x <148755759+Pl-x@users.noreply.github.com>
2024-11-23 18:31:32 +0300

    cabron

---
+++ newboys/conditions.js
@@ -0,0 +1,89 @@
+//this is a new shtick
+let now = new Date()
+function session(launch_codes, authorisation, clearance_level) {
+    if (launch_codes === potus_codes && authorisation === "DEFCON II" && clearance_level === 'travers X56Y6') {
+        initiatelaunch(true)
+        console.log("clear to launch")
+        do {
+            countdown_start(time)
+            set_target(coordinates)
+            detonation_start(start)
+            time++
+        }
+        while (onlogin(true))
+    }
+    else {
+        console.log('launch cannot proceed')
+    }
+}
+
+
+let user =
+{
+    login: 'rebecca',
+    username: 'rebecca67@plx',
+    password: '4596rfssf@87',
+    Age: '17',
+    location: 'kenya',
+    movies:
+        [
+            { title: 'The redemption', revenue: '567,907,986,273' },
+            { title: 'Adillaia', revenue: '587,576,908,878' }
+        ],
+
+
+    logmovies() {
+        console.log(`this are the movies made by ${user.names}`)
+        this.movies.forEach(dispatch => { console.log(dispatch.title, dispatch.revenue) })
+    }
+
+}
+
+function onlogin(password, login, username) {
+
+    if
+        (user.location === 'kenya' && password === user.password && password === (user.password).includes(number) && login === user.login && username === user.username && username === (user.username).includes('@plx')) {
+        return onlogin()
+        console.log("login succesfull", logmovies())
+    }
+    else if
+        (password !== user.password && username === user.username) {
+        return onlogin()
+        console.log(`hello ${user.username} ,please visit the IT department for further assistance`)
+    }
+}
+//new shtick
+function initiatelaunch(publickey, publickey2)
+{
+    console.log("please enter the pair keys in the console opening")
+    return masterkey === publickey + publickey2
+}
+function countdown_start()
+{
+    
+    return time = (`${now.getHours(0)},${now.getMinutes(10)},${now.getSeconds(30)}`)
+    
+}
+function set_target(coordinates)
+{
+  coordinates === latlong
+}
+function detonation_start(start)
+{
+    start === detonationsequence
+}
+
+function getUserInput()
+{
+    return latlong = document.getElementById("Enter Coordinates").value
+    return publickey = document.getElementById("Enter publickey1").value
+    return publickey2 = document.getElementById("Enter publickey2").value
+    return detonationsequence = document.getElementById("Enter the Pick codes").value
+    
+}
+
+let publickey = 25475095648
+let publickey2 = 254794869383
+let coordinates = "45E,67.5N"
+let detonationsequence = 1234567890
+let masterkey = 509545565031
